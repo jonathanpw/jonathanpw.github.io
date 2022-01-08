15760877580955053ec3edac1441df2218cc2546
jonathanpw <jpw338@gmail.com>
2022-01-08 18:13:22 -0500

    updates

---
+++ ST495/lastname_homework_number.r
@@ -0,0 +1,18 @@
+# ST 495 Homework ##
+# First name Last name
+
+# Do not write beyond 80 characters per line.
+# Write your solutions to the homework questions as a combinatin of code and
+# comments.  Alternatively, you could write your solutions as a markdown file. 
+
+# -----------------------------------------------------------------------------
+# Load any packages here, and only here:
+
+# -----------------------------------------------------------------------------
+
+
+
+# -----------------------------------------------------------------------------
+# Question 1.
+
+# -----------------------------------------------------------------------------
