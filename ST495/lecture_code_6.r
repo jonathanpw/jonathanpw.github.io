e5d595b89ed43ee9b2d5d3895e2d61b185c55ff5
jonathanpw <jpw338@gmail.com>
2022-02-16 21:06:06 -0500

    updates

--- ST495/lecture_code_6.r
+++ ST495/lecture_code_6.r
@@ -2,9 +2,9 @@
 # assuming Gaussian errors
 
 
-n = 100
+#n = 100
 #n = 300
-#n = 1000
+n = 1000
 p = 4
 sigma = .25
 #sigma = 1
@@ -18,31 +18,35 @@ XtX_inv = solve(t(X) %*% X)
 # Simulate a large number of data sets and least squares estimators 
 #num_sims = 100
 #num_sims = 300
-num_sims = 1000
+num_sims = 10000
 beta_hat_mat = matrix( NA, nrow=num_sims, ncol=p)
 for(k in 1:num_sims){
 	y = X %*% beta + rnorm( n, mean=0, sd=sigma)
-	beta_hat_mat[k,] = solve(t(X) %*% X) %*% t(X) %*% y
+	beta_hat_mat[k,] = XtX_inv %*% t(X) %*% y
 }
 
 
 # Plot the sampling distributions of the least squares estimates
-#pdf(paste0("sampling_dist_n",n,"_num_sims",num_sims,".pdf"))
+
+pdf(paste0("sampling_dist_n",n,"_num_sims",num_sims,".pdf"))
 par(mfrow=c(2,2))
 for(j in 1:p){
-	hist( beta_hat_mat[,j], freq=F, main=paste0("beta_",j), xlab=NULL)
+	
+	grid = seq( beta[j]-.5, beta[j]+.5, by=.01)
+	
+	hist( beta_hat_mat[,j], freq=F, main=paste0("beta_hat_",j), xlab=NULL,
+	      xlim=c(beta[j]-.5,beta[j]+.5), breaks=floor(sqrt(n)))
 	abline( v=beta[j], col="green", lwd=3)
 	
 	sd_beta_j = sigma * sqrt(XtX_inv[j,j])
-	grid = seq( beta[j]-6*sigma, beta[j]+6*sigma, by=.01)
 	lines(grid, dnorm( grid, mean=beta[j], sd=sd_beta_j), lwd=3)
 }
-#dev.off()
+dev.off()
 
 
 # Check whether confidence intervals for the least squares estimates have the
 # correct coverage
-level = .95
+level = .99
 coverage = rep( 0, p)
 for(k in 1:num_sims){
 	for(j in 1:p){
